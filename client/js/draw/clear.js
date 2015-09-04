e67712178a6813f7efb3de1d4e34917350c9ab9b
fatfisz <fatfisz@gmail.com>
2015-09-04 04:35:33 +0200

    Fixed clearing bug.

--- client/js/draw/clear.js
+++ client/js/draw/clear.js
@@ -5,5 +5,8 @@ var { context } = require('../utils');
 
 
 module.exports = function clearContext() {
+  context.save();
+  context.setTransform(1, 0, 0, 1, 0, 0);
   context.clearRect(0, 0, width * tileSize, height * tileSize);
+  context.restore();
 };
